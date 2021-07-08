0fe73c6d1e4f60ff7b0355271d7a2dec0905c593
Jader Martins <jadermcs94@gmail.com>
2021-07-07 22:51:07 -0300

    update dqn

--- dqntrain.py
+++ dqntrain.py
@@ -1,9 +1,37 @@
-import torch
-import argparse
 import layout
 import numpy as np
+import torch
+from torch import nn
 # import matplotlib.pyplot as plt
 from pacman import ClassicGameRules
+
+BATCH = 64
+LR = 0.0001
+EPOCH = 10000
+EPSILON = 0.5
+memory = list()
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 8, 3)
+        self.conv2 = nn.Conv2d(8, 8, 2, stride=2)
+        self.conv3 = nn.Conv2d(8, 16, 3)
+        self.flatten = nn.Flatten()
+        self.linear1 = nn.Linear(224, 32)
+        self.linear2 = nn.Linear(32, 4)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.flatten(x)
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        return x
+
 
 def color(char):
     if char == '%':# wall
@@ -22,12 +50,11 @@ def color(char):
 def convert_to_image(state):
     text = str(state.data).split('\n')[:-2]
     width, height = len(text[0]), len(text)
-    image = np.zeros((width, height, 3), dtype=np.uint8)
+    image = np.zeros((height, width, 3), dtype=np.float32)
     for i in range(width):
         for j in range(height):
-            image[i,j] = color(text[j][i])
-    return image
-
+            image[j,i] = color(text[j][i])
+    return np.moveaxis(image, -1, 0)/255.0
 
 def train(layout, display):
     import pacmanAgents, ghostAgents, textDisplay
@@ -38,10 +65,23 @@ def train(layout, display):
     else:
         gameDisplay = display
         rules.quiet = False
-    agents = [pacmanAgents.DQNAgent()] + [ghostAgents.RandomGhost(i+1) for i in range(layout.getNumGhosts())]
+    agents = [pacmanAgents.DQNAgent()] +\
+        [ghostAgents.RandomGhost(i+1) for i in range(layout.getNumGhosts())]
     game = rules.newGame(layout, agents[0], agents[1:], gameDisplay)
     state = game.state
-    image = convert_to_image(state)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = CNNModel().to(device)
+    done = False
+    while not done:
+        if np.random.uniform() <= EPSILON:
+            continue
+            action = np.random.choice()
+        else:
+            image = convert_to_image(state)
+            image = torch.tensor(image).unsqueeze(0).to(device)
+            q = model(image)
+            action = np.argmax(q[0].cpu().numpy())
+            done = True
     # fig = plt.figure()
     # ax = fig.add_subplot(1, 1, 1)
     # ax.imshow(image)
